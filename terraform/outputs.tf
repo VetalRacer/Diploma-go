0d86d26ab0c2c24777a66812dca704200f269837
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-06 03:01:18 +0400

    test output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,10 @@ output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
 
+output "registry_password" {
+  value = azurerm_container_registry.default.password
+}
+
 # output "host" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.host
 # }
--- terraform/registry.tf
+++ terraform/registry.tf
@@ -3,5 +3,5 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.default.name
   location            = azurerm_resource_group.default.location
   sku                 = "Standard"
-  admin_enabled       = false
+  admin_enabled       = true
 }
