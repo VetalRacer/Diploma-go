81e2001b36e51afacd8641e35b4ae9181357091a
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-05 21:27:18 +0400

    fix deploy aks

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
 #Service Principle for AKS
-variable "service_principal_client_id" {
-  default = "5df14d85-f3e9-4932-a4b2-93c80c15de47"
-}
-
-variable "service_principal_client_secret" {
-  default = "KFQe0b0Vi6usAcNzat0cbu1~oy0ufeQnD7"
-}
-
 variable "arm_client_id" {}
 variable "arm_client_secret" {}
