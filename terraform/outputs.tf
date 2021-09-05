47a3e476981738e59beb859e80dcc5166f20c790
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-05 19:23:46 +0400

    test15

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,11 @@ output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
 
+output "blah" {
+    value = "${var.service_principal_client_id}"
+}
+
+
 # output "host" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.host
 # }
