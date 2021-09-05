9d2918c7d8e5a1e23f90935b25b35c63631c5c7f
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-05 21:22:52 +0400

    test31

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,6 @@ variable "service_principal_client_id" {
 variable "service_principal_client_secret" {
   default = "KFQe0b0Vi6usAcNzat0cbu1~oy0ufeQnD7"
 }
+
+variable "arm_client_id" {}
+variable "arm_client_secret" {}
