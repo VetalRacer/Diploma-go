2109c9dab684c1acd3b8adc529b6d4582fc665bb
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-05 19:10:34 +0400

    test13

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 #Service Principle for AKS
 variable "service_principal_client_id" {
-  default = "__ARM_CLIENT_ID__"
+  default = __ARM_CLIENT_ID__
 }
 
 variable "service_principal_client_secret" {
-  default = "__ARM_CLIENT_SECRET__"
+  default = __ARM_CLIENT_SECRET__
 }
