9f449bab7b016f18704d81016f3aad92ceb59e8f
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-10 03:26:27 +0400

    test

--- terraform/app/app.tf
+++ terraform/app/app.tf
@@ -1,3 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.default.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+  }
+}
+
 resource "helm_release" "app" {
   name       = "app"
   chart      = "../../deploy/helm-charts/app/nhl"
