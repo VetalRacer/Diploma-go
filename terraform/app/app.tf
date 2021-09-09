55bf8b8d0b134c7adc7b7502cf1f70695bf8cd7a
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-10 01:41:47 +0400

    add app terraform deploy

---
+++ terraform/app/app.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "app" {
+  name       = "app"
+  chart      = "./deploy/helm-charts/app/nhl"
+  values     = "./deploy/helm-charts/app/nhl/values.dev.yaml"
+  namespace  = "develop"
+  force_update = "true"
+  atomic = "true"
+}
