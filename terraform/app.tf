c3ad5df0150b5c0a84b849821da755d7ec7bf617
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-10 02:52:38 +0400

    test

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "app" {
   name       = "app"
   chart      = "../deploy/helm-charts/app/nhl"
-  #values     = "values.dev.yaml"
+  values     = "values.dev.yaml"
   namespace  = "develop"
   force_update = "true"
   atomic = "true"
