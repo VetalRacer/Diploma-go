4d24a6639fba531e3f434469c4a9d86225099bda
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-10 03:32:37 +0400

    test

--- terraform/app/app.tf
+++ terraform/app/app.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+
+  backend "azurerm" {
+  }
+
+}
+
 provider "azurerm" {
   features {}
 }
