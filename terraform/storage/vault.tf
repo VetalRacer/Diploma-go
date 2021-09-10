ff4b6b324059865ad1f5b14196016e3b5a38e325
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-10 21:57:20 +0400

    destroy

---
+++ terraform/storage/vault.tf
@@ -0,0 +1,26 @@
+resource "azurerm_key_vault" "default" {
+  name                       = "diplomakeyvault"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standart"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+    ]
+
+    secret_permissions = [
+      "set",
+      "get",
+      "delete",
+      "purge",
+      "recover"
+    ]
+  }
+}
