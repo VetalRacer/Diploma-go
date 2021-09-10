de0eda762609479ca8f9a855e0e60d590c0f6479
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-11 00:45:39 +0400

    firstdeploy

--- terraform/storage/vault.tf
+++ terraform/storage/vault.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault" "default" {
   name                       = "diplomakeyvault"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
+  location                   = azurerm_resource_group.default.location
+  resource_group_name        = azurerm_resource_group.default.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standart"
   soft_delete_retention_days = 7
