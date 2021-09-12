3d16d8f70234ba197c81fdfaf30328879ec208b9
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-12 22:47:35 +0400

    test

--- terraform/app/app.tf
+++ terraform/app/app.tf
@@ -26,15 +26,14 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "app" {
-  name       = "app"
-  chart      = "../../deploy/helm-charts/app/nhl"
-  values     = [templatefile("../../deploy/helm-charts/app/nhl/values.dev.yaml", {})]
-  namespace  = "develop"
-  force_update = "true"
-  atomic = "true"
-
-}
+#resource "helm_release" "app" {
+#  name       = "app"
+#  chart      = "../../deploy/helm-charts/app/nhl"
+#  values     = [templatefile("../../deploy/helm-charts/app/nhl/values.dev.yaml", {})]
+#  namespace  = "develop"
+#  force_update = "true"
+#  atomic = "true"
+#}
 
 
 data "azurerm_key_vault_secret" "hostk8s" {
