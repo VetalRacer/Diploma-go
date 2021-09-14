730768487a1404801e77d229a45613915cea71b4
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-15 01:15:25 +0400

    fix code

--- internal/database/config.go
+++ internal/database/config.go
@@ -4,8 +4,8 @@ import (
 	"os"
 )
 
-var dbhost, err = os.LookupEnv("DB_HOST")
-var dblogin, err = os.LookupEnv("DB_LOGIN")
-var dbpass, err = os.LookupEnv("DB_PASS")
+var dbhost, _ = os.LookupEnv("DB_HOST")
+var dblogin, _ = os.LookupEnv("DB_LOGIN")
+var dbpass, _ = os.LookupEnv("DB_PASS")
 
 var dbDSN = "postgres://" + dblogin + ":" + dbpass + dbhost + ":5432/nhl"
