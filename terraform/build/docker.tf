3712d0e8573b7035d0ab74332cce2715f3ebe5b9
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 10:55:11 +0400

    test

---
+++ terraform/build/docker.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+
+  backend "azurerm" {
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_key_vault_secret" "registrypass" {
+  name         = "registrypass"
+  key_vault_id = "/subscriptions/29ce556b-5437-4c4b-97d1-4b2730cda3ef/resourceGroups/Diploma-rg/providers/Microsoft.KeyVault/vaults/diplomavault"
+}
+
+provisioner "local-exec" {
+  command = "export PASSSSS=data.azurerm_key_vault_secret.registrypass.value"
+}
