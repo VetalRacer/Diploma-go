b800dd076aa239881040cc8cd7ea1ad89c377b7b
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 23:25:12 +0400

    fix2

--- internal/database/config.go
+++ internal/database/config.go
@@ -8,4 +8,4 @@ var dbhost, _ = os.LookupEnv("DB_HOST")
 var dblogin, _ = os.LookupEnv("DB_LOGIN")
 var dbpass, _ = os.LookupEnv("DB_PASS")
 
-var dbDSN = "postgres://" + dblogin + ":" + dbpass + '@' + dbhost + ":5432/nhl"
+var dbDSN = "postgres://" + dblogin + ":" + dbpass + "@" + dbhost + ":5432/nhl"
