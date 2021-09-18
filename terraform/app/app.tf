c3f21f666d208e59f8eacec8fc093559960664b3
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 18:20:03 +0400

    test

--- terraform/app/app.tf
+++ terraform/app/app.tf
@@ -34,10 +34,10 @@ resource "helm_release" "app" {
   force_update = "true"
   atomic = "true"
 
-  set {
-    name  = "db_pass"
-    value = "${base64encode("${var.db_pass}")}"
-  }
+  #set {
+  #  name  = "db_pass"
+  #  value = "${base64encode("${var.db_pass}")}"
+  #}
 
 
 }
@@ -66,5 +66,6 @@ data "azurerm_key_vault_secret" "clientcacert" {
 
 variable "db_pass" {
   description = "The Password associated with the administrator_login for the PostgreSQL Server."
+  values = "H@Sh1CoR3!@"
   type        = string
 }
