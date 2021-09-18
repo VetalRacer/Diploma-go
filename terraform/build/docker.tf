bd14a6f4af93cc8a21e611ae3467241b514156c4
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 14:30:06 +0400

    test push image

--- terraform/build/docker.tf
+++ terraform/build/docker.tf
@@ -1,4 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
 
+  required_version = ">= 0.14"
+
+  backend "azurerm" {
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
 
 data "azurerm_key_vault_secret" "registrypass" {
   name         = "registrypass"
