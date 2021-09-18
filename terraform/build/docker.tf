ba66791fa057480b0e807892752f0f16ace76144
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 14:10:48 +0400

    test

--- terraform/build/docker.tf
+++ terraform/build/docker.tf
@@ -22,7 +22,7 @@ data "azurerm_key_vault_secret" "registrypass" {
   key_vault_id = "/subscriptions/29ce556b-5437-4c4b-97d1-4b2730cda3ef/resourceGroups/Diploma-rg/providers/Microsoft.KeyVault/vaults/diplomanhlvault"
 }
 
-output "example1" {
+output "registrypass" {
   #value = nonsensitive(data.azurerm_key_vault_secret.registrypass.value)
   value = data.azurerm_key_vault_secret.registrypass.value
   sensitive = true
