3aa0f5790f8cb7340662718d3a99f5823ab4137d
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 11:29:35 +0400

    test

--- terraform/build/docker.tf
+++ terraform/build/docker.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 
 data "azurerm_key_vault_secret" "registrypass" {
   name         = "registrypass"
-  key_vault_id = "/subscriptions/29ce556b-5437-4c4b-97d1-4b2730cda3ef/resourceGroups/Diploma-rg/providers/Microsoft.KeyVault/vaults/diplomavault"
+  key_vault_id = "/subscriptions/29ce556b-5437-4c4b-97d1-4b2730cda3ef/resourceGroups/Diploma-rg/providers/Microsoft.KeyVault/vaults/diplomanhlvault"
 }
 
 resource "null_resource" "cluster" {
