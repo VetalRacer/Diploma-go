bfc184eb4f1da5f1b77d7230ed44ba280f83948f
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-18 12:18:31 +0400

    test

--- terraform/build/docker.tf
+++ terraform/build/docker.tf
@@ -22,19 +22,6 @@ data "azurerm_key_vault_secret" "registrypass" {
   key_vault_id = "/subscriptions/29ce556b-5437-4c4b-97d1-4b2730cda3ef/resourceGroups/Diploma-rg/providers/Microsoft.KeyVault/vaults/diplomanhlvault"
 }
 
-resource "null_resource" "cluster" {
-  provisioner "local-exec" {
-    #command = "export PASSSSS = data.azurerm_key_vault_secret.registrypass.value"
-    #command = "echo data.azurerm_key_vault_secret.registrypass.value >> private_ips.txt"
-    command = "/bin/bash touch test.txt"
-  }
-
-    provisioner "file" {
-    source      = "configs.d"
-    destination = "."
-  }
-}
-
 output "example1" {
-  value = "hello"
+  value = data.azurerm_key_vault_secret.registrypass.value
 }
