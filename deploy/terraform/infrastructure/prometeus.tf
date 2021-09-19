638efde5d0f89cdd729a8d1ae70758d14b13879f
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-20 02:39:45 +0400

    test deploy

---
+++ deploy/terraform/infrastructure/prometeus.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+
+  backend "azurerm" {
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.default.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "app" {
+  name       = "kube-prometheus-stack"
+  repository = "https://github.com/prometheus-community/helm-charts/tree/main/charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = monitoring
+  #reuse_values = "true"
+  #atomic = "true"
+}
