e35c633d347b7dc8bbb263d13151a95be57252c5
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-25 00:55:10 +0400

    deploy

--- deploy/terraform/infrastructure/aks-cluster.tf
+++ deploy/terraform/infrastructure/aks-cluster.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_D4_v3"
+    vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
   }
 
