c83d42c5db705da3d7c11920557443266e08f968
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-09-29 17:48:02 +0400

    test

--- deploy/terraform/infrastructure/elastic.tf
+++ deploy/terraform/infrastructure/elastic.tf
@@ -50,20 +50,19 @@ resource "helm_release" "filebeat" {
 
   set {
     name  = "filebeatConfig"
-    value = " filebeatConfig:
-    filebeat.yml: |
-      filebeat.inputs:
-      - type: container
-        paths:
-          - /var/log/containers/*.log
-        processors:
-        - add_kubernetes_metadata:
-            host: ${NODE_NAME}
-            matchers:
-            - logs_path:
-                logs_path: "/var/log/containers/"
-      output.elasticsearch:
-        host: '${NODE_NAME}'
+    value = "filebeat.yml: |\
+      filebeat.inputs:\
+      - type: container\
+        paths:\
+          - /var/log/containers/*.log\
+        processors:\
+        - add_kubernetes_metadata:\
+            host: ${NODE_NAME}\
+            matchers:\
+            - logs_path:\
+                logs_path: "/var/log/containers/"\
+      output.elasticsearch:\
+        host: '${NODE_NAME}'\
         hosts: '${ELASTICSEARCH_HOSTS:elasticsearch-master:9200}'"
   }
 
