1abdf0b26212a1b44281d3339cf6a9bb1e9f502a
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-10-02 16:26:50 +0400

    fix name

--- deploy/terraform/infrastructure/ingress-controller.tf
+++ deploy/terraform/infrastructure/ingress-controller.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress-controller" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  namespace  = "inginx-ingress"
+  namespace  = "nginx-ingress"
   reuse_values = "true"
   atomic = "true"
 }
