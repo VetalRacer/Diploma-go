115e73136dc4232678ae7de12d487d5bd351b9ed
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-10-03 22:39:41 +0400

    deploy test

--- deploy/terraform/infrastructure/ingress-controller.tf
+++ deploy/terraform/infrastructure/ingress-controller.tf
@@ -2,6 +2,7 @@ resource "helm_release" "ingress-controller" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
+  values     = ["${file("files/values/nginx-ingress.yml")}"]
   namespace  = "nginx-ingress"
   reuse_values = "true"
   atomic = "true"
@@ -10,11 +11,4 @@ resource "helm_release" "ingress-controller" {
     name  = "controller.service.loadBalancerIP"
     value = "20.76.245.249"
   }
-
-  set {
-    name  = "controller.service.annotations\\.service\.beta\.kubernetes\.io/azure-load-balancer-resource-group"
-    value = "Diploma-rg"
-    type  = "string"
-  }
-
 }
