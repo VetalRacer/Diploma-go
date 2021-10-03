a7c07c9fedc27d2a673a774de124189fdc03f672
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-10-04 02:23:38 +0400

    fit test deploy

--- deploy/terraform/infrastructure/ingress-controller.tf
+++ deploy/terraform/infrastructure/ingress-controller.tf
@@ -2,7 +2,6 @@ resource "helm_release" "ingress-controller" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  values     = ["${file("files/values/nginx-ingress.yml")}"]
   namespace  = "nginx-ingress"
   reuse_values = "true"
   atomic = "true"
