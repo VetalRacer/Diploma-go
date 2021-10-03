cecacd52b78cb38cab75de4a8e349c66aaf512a8
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-10-04 02:00:00 +0400

    test

--- deploy/terraform/infrastructure/aks-cluster.tf
+++ deploy/terraform/infrastructure/aks-cluster.tf
@@ -12,6 +12,7 @@ resource "azurerm_public_ip" "default" {
   resource_group_name = azurerm_resource_group.default.name
   location            = azurerm_resource_group.default.location
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
