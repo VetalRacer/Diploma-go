9e9ed2577ee4343ff3111334dc8281d2510ecebe
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-10-06 01:18:37 +0400

    add sonar ingress

--- deploy/terraform/infrastructure/sonar-qube.tf
+++ deploy/terraform/infrastructure/sonar-qube.tf
@@ -17,3 +17,37 @@ esource "helm_release" "sonarqube" {
   reuse_values = "true"
   atomic = "true"
 }
+
+resource "kubernetes_ingress" "sonar_ingress" {
+  metadata {
+    namespace = "quality-gate"
+    name = "sonar-qube-ingress"
+    annotations = {
+      "enable-vts-status" = "true"
+      "kubernetes.io/ingress.class" = "nginx"
+      "nginx.ingress.kubernetes.io/proxy-body-size" = "15m"
+      "nginx.ingress.kubernetes.io/proxy-buffer-size" = "16k"
+      "nginx.ingress.kubernetes.io/ssl-ciphers" = "ALL:!aNULL:!EXPORT56:RC4+RSA:+HIGH"
+      "nginx.org/proxy-hide-headers" = "Server, X-Powered-By, X-AspNet-Version, X-AspNet-Mvc-Version"
+      "nginx.org/server-tokens" = "False"
+      "prometheus.io/port" = "10254"
+      "prometheus.io/scrape" = "true"
+    }
+  }
+
+  spec {
+    rule {
+      host = "sonar-qube.hgest.ru"
+      http {
+        path {
+          backend {
+            service_name = "sonarqube-sonarqube"
+            service_port = 80
+          }
+
+          path = "/"
+        }
+      }
+    }
+  }
+}
